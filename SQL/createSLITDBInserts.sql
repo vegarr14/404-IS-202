050c858a6b129ffb2f33ade1140884a82fdded28
Shammers95 <sondreh@live.no>
2018-10-10 13:30:35 +0200

    Update createSLITDBInserts.sql

--- SQL/createSLITDBInserts.sql
+++ SQL/createSLITDBInserts.sql
@@ -14,18 +14,18 @@ INSERT into Student values('1','Vegar','Ryen','emailxd','12345678'),
 INSERT into Foreleser values ('4','Hans','Mr.olav','yo@yahoo.com','99999999'),
 (5, 'Mr.Vegar','Professor Ryen','emailxd', 12345678);
 
-insert into Kurs values('1','IS-200','Systemanalyse og systemutvikling'),
-('2','IS-201','Datamodellering og databasesystemer'),
-('3','IS-202','Programmeringsprosjekt');
+insert into Kurs values('IS-200','Systemanalyse og systemutvikling'),
+('IS-201','Datamodellering og databasesystemer'),
+('IS-202','Programmeringsprosjekt');
 
-INSERT INTO Modul values ('1','1','4','1','You`re wondering who i am!'),
-('2','1','4','2','machine or mannequin. With parts made in Japan, I am the modern man!');
+INSERT INTO Modul values ('1','IS-202','4','1','You`re wondering who i am!'),
+('2','IS-202','4','2','machine or mannequin. With parts made in Japan, I am the modern man!');
 
-insert into TarKurs values('1','1'),
-('2','1'),
-('3','2');
+insert into TarKurs values('IS-200','1'),
+('IS-201','1'),
+('IS-202','2');
 
-insert into ForeleserKurs values('1','4');
+insert into ForeleserKurs values('IS-200','4');
                  
 INSERT ignore into Innlevering (`modulId`, `id`, `innlevKommentar`, `innlevPoeng`) 
 values (1, 1, 'Funker ting? Hurra', 80),
@@ -37,8 +37,3 @@ INSERT ignore into Kommentarer (`id`, `innlevId`, `komKommentar`)
 values (4, 1, 'Helsa helsa masse');
                      
 /* select * from modulListe; */
-
-/* select innlev_Poeng, forNavn, etterNavn
-from ModulListe, Innlevering, Student
-where Innlevering.innlevId = Student.id
-Group by Student.id; */
