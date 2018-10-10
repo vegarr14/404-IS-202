b5e46aa47122fb67bf60cfea70be7efb989b88c4
Shammers95 <sondreh@live.no>
2018-10-10 11:16:25 +0200

    Update createSLITDB.sql

--- SQL/createSLITDB.sql
+++ SQL/createSLITDB.sql
@@ -3,6 +3,15 @@ drop schema slitdb;
 Create schema if not exists SLITDB;
 USE SLITDB;
 
+drop table if exists Kommentarer;
+drop table if exists Innlevering;
+drop table if exists ModulListe;
+drop table if exists Modul;
+drop table if exists Kurs;
+drop table if exists Foreleser;
+drop table if exists Student;
+drop table if exists Bruker;
+
 CREATE TABLE if not exists`Bruker` (
   `id` int(11) NOT null AUTO_INCREMENT,
   `brukerNavn` varchar(20) NOT NULL,
@@ -32,9 +41,11 @@ CREATE TABLE if not exists`Foreleser` (
 
 CREATE TABLE if not exists `modulListe` (
   `modul_Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `modul_Navn` varchar(20) NOT NULL,
   `modul_Nummer` int (30) NOT NULL,
-  primary key(`modul_Id`)
+  primary key(`modul_Id`),
+  Constraint `FK_Foreleser_ModulListe` Foreign Key (`id`) references `Foreleser`(`id`)
   );
 
 CREATE TABLE if not exists`Kurs` (
@@ -54,6 +65,28 @@ CREATE TABLE if not exists `Modul` (
  constraint `FK_Modul_Kurs` foreign key (`kursId`) references `Kurs` (`id`),
  constraint `FK_Modul_Forelser` foreign key (`foreleserId`) references `Foreleser` (`id`)
  );
+ 
+ CREATE TABLE if not exists `Innlevering` (
+  `innlev_Id` int(11) NOT NULL AUTO_INCREMENT,
+  `modul_Id` int (11) NOT NULL,
+  `modul_Nummer` int (11) NOT NULL,
+  `id` int (11) NOT NULL,
+  `innlev_Kommentar` varchar (250) NOT NULL,
+  `innlev_Poeng` int NOT NULL,
+  primary key(`innlev_Id`),
+  Constraint `FK_ModulListe_Innlevering` Foreign Key (`modul_Id`) references `ModulListe` (`modul_Id`),
+  Constraint `FK_Bruker_Innlevering` Foreign Key (`id`) references `Bruker` (`id`)
+  );
+  
+  CREATE TABLE if not exists `Kommentarer` (
+  `kom_Id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int (11) NOT NULL,
+  `innlev_Id` int(11) NOT NULL,
+  `kom_kommentar` varchar (250) NOT NULL,
+  primary key(`kom_Id`),
+  Constraint `FK_Kommentarer_Bruker` Foreign Key (`id`) references `Bruker` (`id`),
+  Constraint `FK_Kommentarer_Innlev` Foreign Key (`innlev_Id`) references `Innlevering` (`innlev_Id`)
+  );
 
 CREATE TABLE `TarKurs` (
   `kursId` int(6) Not Null,
@@ -69,45 +102,3 @@ CREATE TABLE `ForeleserKurs` (
   Constraint `FK_ForeleserKurs_Kurs` foreign key (`kursId`) references `kurs` (id),
   Constraint `FK_ForeleserKurs_Student` foreign key (`foreleserId`) references `foreleser` (id) 
 );
-
-INSERT into bruker (brukernavn, passord) Values ('test', aes_encrypt('test', 'domo arigato mr.roboto')),
-('test', aes_encrypt('test', 'domo arigato mr.roboto')),
-('test', aes_encrypt('test', 'domo arigato mr.roboto')),
-('test', aes_encrypt('test', 'domo arigato mr.roboto')),
-('test', aes_encrypt('test', 'domo arigato mr.roboto')),
-('test', aes_encrypt('test', 'domo arigato mr.roboto'));
-
-INSERT into Student values('1','Vegar','Ryen','emailxd','12345678'),
-('2','Sondre','Hammersbøen','emailcx','87654321'),
-('3','Erlend','Thorsen','mailmail','12344321');
-
-INSERT into modulListe (`modul_Navn`, `modul_Nummer`) 
-values ('Modul', 1), 
-('Modul', 2), 
-('Modul', 3), 
-('Modul', 4), 
-('Modul', 5);
-                     
-INSERT into Foreleser values ('4','Hans','Mr.olav','yo@yahoo.com','99999999');
-
-insert into Kurs values('1','IS-200','Systemanalyse og systemutvikling'),
-('2','IS-201','Datamodellering og databasesystemer'),
-('3','IS-202','Programmeringsprosjekt');
-
-INSERT INTO Modul values ('1','1','4','1','You`re wondering who i am!'),
-('2','1','4','2','machine or mannequin. With parts made in Japan, I am the modern man!');
-
-insert into TarKurs values('1','1'),
-('2','1'),
-('3','2');
-
-insert into ForeleserKurs values('1','4');
-
-INSERT into modulListe (`modul_Navn`, `modul_Nummer`) 
-values ('Modul', 1), 
-('Modul', 2), 
-('Modul', 3), 
-('Modul', 4), 
-('Modul', 5);
-                     
-/* select * from modulListe; */
