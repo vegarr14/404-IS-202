ea08e0e230afca660d9486c081e6019dc5a2327b
ErlendT <32453298+ErlendT@users.noreply.github.com>
2018-11-13 10:01:34 +0100

    printnot

--- IS-202/src/java/NotifikasjonSystem/PrintNotifikasjoner.java
+++ IS-202/src/java/NotifikasjonSystem/PrintNotifikasjoner.java
@@ -26,16 +26,14 @@ public class PrintNotifikasjoner {
     
     private String melding;
     private String timestamp;
-    private String link;
 
     //Printer alle uleste notifikasjoner for den enkelte bruker
-    public void printUleste(String id, String type, String plassering, JspWriter JW, PrintWriter PW) throws IOException{
+    public void printUleste(String id, String type, JspWriter JW, PrintWriter PW) throws IOException{
         
          ArrayList<String> notArray = new ArrayList<String>();
         rs = query.query("Select * from notifikasjoner WHERE mottakerId="+id+" AND notUlest=1 order by notOpprettet desc");
      
         try {
-            notArray.add("<div class='notifikasjoner' id='"+plassering+"'>");
             notArray.add("<h2>Nye Notifikasjoner</h2>");
             notArray.add("<hr>");
             if(!rs.next()){
@@ -45,17 +43,18 @@ public class PrintNotifikasjoner {
                 rs.previous();
                 while(rs.next()){
                     
+                    String notId = rs.getString(1);
                     timestamp = rs.getString(7);
                     String notType = rs.getString(5);
                     int senderId = rs.getInt(3);
-                    int notReferererId = rs.getInt(6);
+                    String notReferererId = rs.getString(6);
                     
                     melding = produserMelding(notType, senderId, notReferererId);
                     
                     notArray.add("<li>");
                     notArray.add("<div>");
 
-                    notArray.add("<a href='NotifikasjonVideresender?id="+id+"&notType="+notType+"&notRefId="+notReferererId+"'>"+melding+"</a>");
+                    notArray.add("<a href='NotifikasjonVideresender?id="+id+"&notId="+notId+"&notType="+notType+"&notRefId="+notReferererId+"'>"+melding+"</a>");
                     notArray.add("<p>Dette skjedde den <time>"+timestamp+"</time>.</p>");
                     notArray.add("</div>");
                     notArray.add("</li>");
@@ -64,7 +63,6 @@ public class PrintNotifikasjoner {
                 notArray.add("</ul>");
             }
             
-            notArray.add("</div>");
         } catch (SQLException ex) {
             Logger.getLogger(PrintNotifikasjoner.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -83,11 +81,66 @@ public class PrintNotifikasjoner {
                 }
                 
             }
-        query.close();
         
     }
+    public void printLeste(String id, String type, JspWriter JW, PrintWriter PW) throws IOException{
+        
+         ArrayList<String> notArray = new ArrayList<String>();
+        rs = query.query("Select * from notifikasjoner WHERE mottakerId="+id+" AND notUlest=0 order by notOpprettet desc");
+        
+        try {          
+            if(!rs.next()){
+                notArray.add("<h4> Du har ingen gamle notifikasjoner :(</h4>");
+            }else{
+                notArray.add("<h2>Gamle Notifikasjoner</h2>");
+                notArray.add("<hr>");
+                notArray.add("<ul>");
+                rs.previous();
+                while(rs.next()){
+                    
+                    String notId = rs.getString(1);
+                    timestamp = rs.getString(7);
+                    String notType = rs.getString(5);
+                    int senderId = rs.getInt(3);
+                    String notReferererId = rs.getString(6);
+                    
+                    melding = produserMelding(notType, senderId, notReferererId);
+                    
+                    notArray.add("<li>");
+                    notArray.add("<div>");
+
+                    notArray.add("<a href='NotifikasjonVideresender?id="+id+"&notId="+notId+"&notType="+notType+"&notRefId="+notReferererId+"'>"+melding+"</a>");
+                    notArray.add("<p>Dette skjedde den <time>"+timestamp+"</time>.</p>");
+                    notArray.add("</div>");
+                    notArray.add("</li>");
+                    notArray.add("<hr>");
+                }
+                notArray.add("</ul>");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PrintNotifikasjoner.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //velger type Writer og skriver ut navbar
+            if(type == "JSP"){
+                
+                for(String linje: notArray) {
+                    JW.println(linje);
+                }
+                
+            }else{
+              
+                for(String linje: notArray) {
+                    PW.println(linje);
+                }
+                
+            }
+        
+    }
+    
     //Produserer melding basert på type, sender og hvor den blir referert til
-    private String produserMelding(String notType, int senderId, int notReferererId){
+    private String produserMelding(String notType, int senderId, String notReferererId){
         
         ResultSet rs2 = null;
         String senderNavn;
@@ -95,28 +148,65 @@ public class PrintNotifikasjoner {
         String modulNummer;
         
         String s = "Error et eller annet";
-        
-        if(notType.equals("nyModul")){
-            
-            try {
-                rs2 = query.query("Select forNavn, etterNavn from foreleser where id="+senderId+"");
-                rs2.next();
-                senderNavn = rs2.getString(1) + " " + rs2.getString(2);
-                
-                rs2 = query.query("Select kursId, modulNummer from modul where modulId="+notReferererId+"");
+        try {
+            //notifikasjoner til modul
+            if(notType.equals("nyModul") | notType.equals("oppdatertModul" ) | notType.equals("slettetModul")){
+
+                //henter navn til sender
+                senderNavn = getSenderName("foreleser", senderId);
+                if(notType.equals("slettetModul")){
+                    s = senderNavn + " har fjernet en modul. Vi vet desverre ikke hvilken siden den ble slettet #Irony";
+                }else{
+                    //Finner modulnummer og kursid som hører til notifikasjonen
+                    rs2 = query.query("Select kursId, modulNummer from modul where modulId="+notReferererId+"");
+                    rs2.next();
+                    kursId = rs2.getString(1);
+                    modulNummer = rs2.getString(2);
+
+                    if(notType.equals("nyModul")){
+                        s = senderNavn + " har opprettet en ny modul i " + kursId + "<br> Modul " + modulNummer;
+                    }
+                    else if(notType.equals("oppdatertModul")){
+                        s = senderNavn + " har oppdatert modul " + modulNummer + " i " + kursId;
+                    }
+                }
+            }
+            //notifikasjoner til innlevering
+            else if(notType.equals("nyInnlevering")){
+                //henter navn til sender
+                senderNavn = getSenderName("student", senderId);
+                //Finner modulnummer og kursid som hører til notifikasjonen
+                rs2 = query.query("Select kursId, modulNummer from modul where modulId in(select modulId from innlevering where innlevId="+notReferererId+")");
                 rs2.next();
                 kursId = rs2.getString(1);
                 modulNummer = rs2.getString(2);
                 
-                s = senderNavn + " har opprettet en ny modul i " + kursId + "<br> Modul " + modulNummer;
-              
-                link = "Modul?kursId="+kursId+"&modulId="+notReferererId+"";
-            } catch (SQLException ex) {
+                s = senderNavn + " har levert modul " + modulNummer + " i " + kursId;
+            }            
+            //Notifikajsoner om fjenet fra eller lagt tilkurs
+            else if(notType.equals("fjernetFraKurs") | notType.equals("lagtTilKurs")){
+                //henter navn til sender
+                senderNavn = getSenderName("foreleser", senderId);
+                if(notType.equals("fjernetFraKurs")){
+                    s = "Du har blitt fjernet fra " + notReferererId + " av " + senderNavn;
+                }else{
+                    s = "Du er blitt lagt til i kurset " + notReferererId + " av " + senderNavn;
+                }
+                
+            }
+        } catch (SQLException ex) {
                 Logger.getLogger(PrintNotifikasjoner.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-        }
         
             return s;
-    }    
+    }
+    // Henter fornavn og etternavn til sender
+    private String getSenderName(String table, int senderId) throws SQLException{
+        ResultSet rs2 = null;
+        
+        rs2 = query.query("Select forNavn, etterNavn from "+table+" where id="+senderId+"");
+        rs2.next();
+        String s = rs2.getString(1) + " " + rs2.getString(2);
+        return s;
+    }
 }
