73423aa5bf49bfcc0a12e3a742ff836115cf9bba
Sondre <Sondre@10.228.4.179>
2018-11-27 12:35:29 +0100

    Merge origin/Dev into Sondres-masterverk
    Conflicts:
    	IS-202/src/java/Database/Query.java
    	IS-202/src/java/Servlets/BrukerListe.java
    	IS-202/src/java/Servlets/KursListe.java
    	IS-202/src/java/Servlets/LeggTilKurs.java

--- IS-202/src/java/Klasser/Kjeks.java
+++ IS-202/src/java/Klasser/Kjeks.java
@@ -5,20 +5,9 @@
  */
 package Klasser;
 
-import Servlets.Login;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Cookie;
 
 /**
  *
@@ -30,16 +19,10 @@ public class Kjeks {
     }
     
     public void makeCookie (HttpServletRequest request, HttpServletResponse response)    {
-        try {
         if (request.getParameter("huskMeg")!=null)   {
-            Cookie kjeks = new Cookie("kjeks", URLEncoder.encode(request.getParameter("brukernavn"), "UTF-8"));
+            Cookie kjeks = new Cookie("kjeks", request.getParameter("brukernavn"));
             kjeks.setMaxAge(24*60*60);  //Hvor lenge cookien blir lagret i sekunder (24*60*60 = 24 timer)
             response.addCookie(kjeks);
-            System.out.println("Kjeks 1: "+kjeks);
-            System.out.println("KjeksGetName: "+kjeks.getName());
-        }
-        }   catch (UnsupportedEncodingException uEx) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, uEx);
         }
     }
     
@@ -56,5 +39,3 @@ public class Kjeks {
         }
     }
 }
-
-
