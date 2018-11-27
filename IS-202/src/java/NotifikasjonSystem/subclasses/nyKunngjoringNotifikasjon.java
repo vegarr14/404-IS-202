1a2d8c5d1f182869ce6376768b1196314fe08741
felpus <35775929+felpus@users.noreply.github.com>
2018-11-27 01:06:43 +0100

    kunngjøringnotifikasjon bug i github desktop
    ?????????????????

--- IS-202/src/java/NotifikasjonSystem/subclasses/nyKunngjoringNotifikasjon.java
+++ IS-202/src/java/NotifikasjonSystem/subclasses/nyKunngjoringNotifikasjon.java
@@ -5,10 +5,37 @@
  */
 package NotifikasjonSystem.subclasses;
 
+import Database.Query;
+import NotifikasjonSystem.Notifikasjon;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Erlend Thorsen
  */
-public class nyKunngjoringNotifikasjon {
+public class NyKunngjoringNotifikasjon extends Notifikasjon {
     
+    public void getAndSetNyKungjoring(String kursId, int foreleserId){
+        ResultSet rs = null;
+        Query query = new Query();
+        
+        this.sender = foreleserId;
+        this.refererer = kursId;
+        this.type = "nyKunngjoring";
+        this.opprettet = getTimestamp();
+        
+        rs = query.query("SELECT studentId from TarKurs WHERE kursId='"+kursId+"'");
+        try{
+            while(rs.next()){
+                this.mottaker = rs.getInt(1);
+                lagreNotifikasjon(this);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(NyKunngjoringNotifikasjon.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        query.close();
+    }
 }
